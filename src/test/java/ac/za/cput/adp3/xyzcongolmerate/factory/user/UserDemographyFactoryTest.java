db1dd98856b6fa7a83c272b33653cdb77d9b2bcb
Jayde Fraanz <216172489@mycput.ac.za>
2019-09-02 23:32:40 +0200

    initial complete

--- src/test/java/ac/za/cput/adp3/xyzcongolmerate/factory/user/UserDemographyFactoryTest.java
+++ src/test/java/ac/za/cput/adp3/xyzcongolmerate/factory/user/UserDemographyFactoryTest.java
@@ -1,23 +1,22 @@
 package ac.za.cput.adp3.xyzcongolmerate.factory.user;
 
+import ac.za.cput.adp3.xyzcongolmerate.domain.user.UserDemography;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Date;
 
 import static org.junit.Assert.*;
 
 public class UserDemographyFactoryTest {
 
-    //TODO: implement method body ONLY!
+
     @Test
     public void buildUserDemography() {
-        throw new UnsupportedOperationException("Not supported yet.");
-        /**
-         * Your implementation goes here
-         *
-         * INSTRUCTION
-         * 1. Remove line [//TODO: implement method body ONLY!]
-         * 2. Remove line [throw new UnsupportedOperationException("Not yet supported.");]
-         * 3. Test the UserDemographyFactory class
-         * 4. Assert that the an object is created.
-         */
+        UserDemography userDemography =  UserDemographyFactory.buildUserDemography("jay@mail.com","Mr","Male","Clrd",new Date());
+
+        System.out.println(userDemography.toString());
+
+        Assert.assertNotNull(userDemography);
     }
 }
--- src/test/java/ac/za/cput/adp3/xyzcongolmerate/factory/user/UserRoleFactoryTest.java
+++ src/test/java/ac/za/cput/adp3/xyzcongolmerate/factory/user/UserRoleFactoryTest.java
@@ -1,23 +1,20 @@
 package ac.za.cput.adp3.xyzcongolmerate.factory.user;
 
+import ac.za.cput.adp3.xyzcongolmerate.domain.user.UserRole;
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class UserRoleFactoryTest {
 
-    //TODO: implement method body ONLY!
+
     @Test
     public void buildUserRole() {
-        throw new UnsupportedOperationException("Not supported yet.");
-        /**
-         * Your implementation goes here
-         *
-         * INSTRUCTION
-         * 1. Remove line [//TODO: implement method body ONLY!]
-         * 2. Remove line [throw new UnsupportedOperationException("Not yet supported.");]
-         * 3. Test the UserRoleFactory class
-         * 4. Assert that the an object is created.
-         */
+        UserRole userRole = UserRoleFactory.buildUserRole("345","k@mart.com","admin");
+
+        System.out.println(userRole);
+
+        Assert.assertNotNull(userRole);
     }
 }
